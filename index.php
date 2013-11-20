9cfceb468d46b04dce156cc25b6d0f02ebdb5b63
jiaoge008 <wangxianjiao008@163.com>
2013-11-20 22:59:45 +0800

    first upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+	echo date("Y-m-d H:i:s");echo "<br>";
+	echo date("Y-m-d H:i:s");echo time();
+	phpinfo();
+	echo "wo shi yige zhongguo ren";
+	?>
---
+++ testdrive/protected/views/site/contact.php
@@ -0,0 +1,97 @@
+<?php
+/* @var $this SiteController */
+/* @var $model ContactForm */
+/* @var $form CActiveForm */
+
+$this->pageTitle=Yii::app()->name . ' - Contact Us';
+$this->breadcrumbs=array(
+	'联系我们',
+);
+?>
+
+<h1>Contact Us</h1>
+
+<?php if(Yii::app()->user->hasFlash('contact')): ?>
+
+<div class="flash-success">
+	<?php echo Yii::app()->user->getFlash('contact'); ?>
+</div>
+
+<?php else: ?>
+
+<p>
+If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
+</p>
+<table >
+  <tr>
+    <th><font color='red'>用户姓名</font></th>
+    <th>上次登陆时间</th>
+  </tr>
+  <?php foreach($users as $key => $val):?>
+  <tr>
+  <td  bgcolor="#ffffff"><font color='red'><?php echo $val['userName'];?></font></td>
+  <td><?php echo CHtml::link(date("Y-m-d H:i:s",$val['lastLoginTime']),array('site/index'))?></td>
+  </tr>
+  <?php endforeach;?>
+</table>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'contact-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+)); ?>
+<input type='button' id='submit' value='提交'>
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+<?php echo "hahhahahahaha"; ?>
+	<?php echo $form->errorSummary($model); ?>
+<?php echo "hahhahahahaha"; ?>
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textField($model,'name'); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email'); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'subject'); ?>
+		<?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($model,'subject'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'body'); ?>
+		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'body'); ?>
+	</div>
+
+	<?php if(CCaptcha::checkRequirements()): ?>
+	<div class="row">
+		<?php echo $form->labelEx($model,'verifyCode'); ?>
+		<div>
+		<?php $this->widget('CCaptcha'); ?>
+		<?php echo $form->textField($model,'verifyCode'); ?>
+		</div>
+		<div class="hint">Please enter the letters as they are shown in the image above.
+		<br/>Letters are not case-sensitive.</div>
+		<?php echo $form->error($model,'verifyCode'); ?>
+	</div>
+	<?php endif; ?>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Submit'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
+
+<?php endif; ?>
